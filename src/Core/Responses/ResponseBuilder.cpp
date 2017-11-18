9fea91e31da16d22d92df670a3c35532e5eaf414
Alessandro Manno <alexmanno96@gmail.com>
2017-11-18 16:35:08 +0100

    Removed logic from ResponseBuilder

--- src/Core/Responses/ResponseBuilder.cpp
+++ src/Core/Responses/ResponseBuilder.cpp
@@ -5,13 +5,5 @@
 #include "ResponseBuilder.h"
 
 std::string ResponseBuilder::buildRawResponse(Response response) {
-
-    std::string raw = "HTTP/1.1 " + response.status + " OK\n"+
-            "Connection: close\n"+
-            "Content-type: text/html; charset=UTF-8\n"+
-            "\n";
-
-    std::strcat(raw, "");
-
     return std::string();
 }
